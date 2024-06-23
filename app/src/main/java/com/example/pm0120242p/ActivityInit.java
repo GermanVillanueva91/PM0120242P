bbc67ebc275d586ccf480e4ba6bdf555b2ab2d29
GermanVillanueva91 <xander_sn_91@hotmail.com>
2024-06-22 19:15:04 -0600

    Ingreso de datos personales y saludo con toast

--- app/src/main/java/com/example/pm0120242p/ActivityInit.java
+++ app/src/main/java/com/example/pm0120242p/ActivityInit.java
@@ -1,6 +1,11 @@
 package com.example.pm0120242p;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActivityInit extends AppCompatActivity {
+public class ActivityInit extends AppCompatActivity
+{
 
+    EditText nombres, apellido, edad, correo;
+
+    Button btningresar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,24 @@ public class ActivityInit extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        nombres = (EditText) findViewById(R.id.nombres);
+        edad = (EditText) findViewById(R.id.edad);
+        apellido = (EditText) findViewById(R.id.apellido);
+        correo = (EditText) findViewById(R.id.correo);
+        btningresar = (Button) findViewById(R.id.btningresar);
+
+        btningresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Agregar();
+            }
+
+            private void Agregar()
+            {
+                Toast.makeText(ActivityInit.this, "Hola como estas", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
